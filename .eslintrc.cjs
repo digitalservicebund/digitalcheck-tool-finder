8ee6a706d53da2391c4c7efa5dcfdffe4bc56a30
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-11-09 10:42:16 +0100

    Update eslint config
    React import not needed in each file for React v17+.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -28,6 +28,7 @@ module.exports = {
   },
   plugins: ["@typescript-eslint", "react", "react-refresh"],
   rules: {
+    "react/react-in-jsx-scope": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
