180f1d7c489afdc00fc12eb0ea4a82fc2b6a06ce
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-11-17 21:55:10 +0100

    Add routing to allow multiple pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/PageHeader";
 import Footer from "./components/Footer";
 import FeedbackBanner from "./components/FeedbackBanner";
 
+import Home from "./routes/Home";
+
 function App() {
   return (
-    <>
-      <body className="flex flex-col min-h-screen">
-        {/*<CookieBanner hasTrackingConsent={hasTrackingConsent} content={cookieBannerContent} ip={ip} />*/}
-        <Header />
-        <main className="flex-grow">
-          <h1 className="ds-heading-01-reg mb-40 text-center">
-            Digitalcheck Werkzeugfinder
-          </h1>
-          {/*<Outlet />*/}
-          <FeedbackBanner />
-        </main>
-        <Footer />
-      </body>
-    </>
+    <body className="flex flex-col min-h-screen">
+      {/*<CookieBanner hasTrackingConsent={hasTrackingConsent} content={cookieBannerContent} ip={ip} />*/}
+      <Header />
+      <main className="flex-grow">
+        <Routes>
+          <Route path={"/"} element={<Home />} />
+        </Routes>
+        <FeedbackBanner />
+      </main>
+      <Footer />
+    </body>
   );
 }
 
---
+++ src/routes/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <h1 className="ds-heading-01-reg mb-40 text-center">
+      Digitalcheck Werkzeugfinder
+    </h1>
+  );
+}
+
+export default Home;
