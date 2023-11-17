28e08d2435c5ee40e8cc61ac149d93641cf08a43
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-11-17 21:55:57 +0100

    Add header to home page

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,8 +1,24 @@
+import Container from "../components/Container";
+import Background from "../components/Background";
+import Header from "../components/Header";
+
 function Home() {
   return (
-    <h1 className="ds-heading-01-reg mb-40 text-center">
-      Digitalcheck Werkzeugfinder
-    </h1>
+    <Background backgroundColor="blue" paddingTop="48" paddingBottom="48">
+      <Container paddingTop="0" paddingBottom="0">
+        <Header
+          heading={{
+            tagName: "h1",
+            look: "ds-heading-01-reg",
+            text: "Werkzeugfinder für Visualisierungen",
+          }}
+          content={{
+            markdown: `Hier erfahren Sie, welche Art der Visualisierung und welches Werkzeug für Ihr Bedürfnis das 
+            richtige ist. Beantworten Sie uns drei Fragen und wir schlagen Ihnen das passende Werkzeug vor.`,
+          }}
+        ></Header>
+      </Container>
+    </Background>
   );
 }
 
