37e2f17449c29a009e5f0c351d62c6f7cec600d5
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-11-22 09:42:38 +0100

    Cleanup and fix e2e tests

--- test/e2e/example.spec.ts
+++ test/e2e/example.spec.ts
@@ -1,15 +1,13 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("basic example test", () => {
-  test("has title", async ({ page }) => {
+test.describe("test page", () => {
+  test("page has title", async ({ page }) => {
     await page.goto("/");
     await expect(page).toHaveTitle("Digitalcheck Werkzeugfinder");
   });
 
-  test("shows hello message", async ({ page }) => {
+  test("page has h1", async ({ page }) => {
     await page.goto("/");
-    await expect(
-      page.locator("text=Visualisieren im Digitalcheck"),
-    ).toBeVisible();
+    await expect(page.locator("h1.ds-heading-01-reg")).toBeVisible();
   });
 });
