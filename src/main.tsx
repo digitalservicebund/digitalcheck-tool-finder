11a7fdb778c1a9626fbcbfdc3165f698f0c951dc
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-11-23 16:47:05 +0100

    Switch to HashRouter
    GitHub Pages deployment does not support the functionallity of BrowserRouter which leads to 404 errors when reloading the page on subroutes.
    See more here: https://stackoverflow.com/questions/27928372/react-router-urls-dont-work-when-refreshing-or-writing-manually

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./style.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const basename: string = import.meta.env.VITE_PATH_BASENAME ?? "";
