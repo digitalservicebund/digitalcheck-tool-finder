35f0921c1b75375019620071af0470569374e2b2
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-11-27 14:46:00 +0100

    Remove "Home" from breadcrumbs

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -29,17 +29,9 @@ export default function Breadcrumbs({ breadcrumbs }: BreadcrumbsProps) {
   return (
     validBreadcrumbs.length > 0 && (
       <nav className="py-8 px-16 bg-blue-100 flex flex-wrap items-center text-base">
-        {/* Note: can't use <Link> or <NavLink> as we require fresh data from the root loader */}
-        <Link
-          to="/"
-          aria-label="Startseite"
-          className="focus:outline ds-link-01-bold"
-        >
-          Home
-        </Link>
         {validBreadcrumbs.map((breadcrumb, idx, arr) => (
           <div key={breadcrumb.title}>
-            <span className="mx-8">/</span>
+            {idx !== 0 ? <span className="mx-8">/</span> : ""}
             <span>
               {idx === arr.length - 1 ? (
                 <span>{breadcrumb.title}</span>
