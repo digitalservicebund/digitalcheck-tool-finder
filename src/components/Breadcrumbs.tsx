2b0153d4265cc1c4832247113f21778b81ddff32
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-11-29 16:50:20 +0100

    Add Home link to breadcrumbs

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { z } from "zod";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 
 export const BreadcrumbPropsSchema = z.object({
   url: z.string(),
@@ -39,9 +40,16 @@ export default function Breadcrumbs({ breadcrumbs }: BreadcrumbsProps) {
   return (
     filteredBreadcrumbs.length > 0 && (
       <nav className="py-8 px-16 bg-blue-100 flex flex-wrap items-center text-base">
+        <Link
+          to="/"
+          aria-label="Startseite"
+          className="focus:outline ds-link-01-bold"
+        >
+          <HomeOutlinedIcon className="!h-[1.6rem] !w-[1.6rem]" />
+        </Link>
         {filteredBreadcrumbs.map((breadcrumb, idx, arr) => (
           <div key={breadcrumb.title}>
-            {idx !== 0 ? <span className="mx-8">/</span> : ""}
+            <span className="mx-8">/</span>
             <span>
               {idx === arr.length - 1 ? (
                 <span>{breadcrumb.title}</span>
