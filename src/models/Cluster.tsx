c450f516d847c534bd1a904d497beb94d446cea9
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-12-14 10:53:59 +0100

    Add more data
    Cluster description and image, Tool image and properties for description and link.

--- src/models/Cluster.tsx
+++ src/models/Cluster.tsx
@@ -1,8 +1,11 @@
 import { Entity } from "./Entity";
+import { Image } from "./Image";
 
 export interface Cluster extends Entity {
   id: string;
   name: string;
   tools: string[];
   notations: string[];
+  description: string;
+  img: Image;
 }
--- src/models/Tool.tsx
+++ src/models/Tool.tsx
@@ -1,8 +1,12 @@
 import { Entity } from "./Entity";
+import { Image } from "./Image";
 
 export interface Tool extends Entity {
   id: string;
   name: string;
+  description: string;
+  link: string;
   fidelity: string;
   ressorts: string[];
+  img: Image;
 }
