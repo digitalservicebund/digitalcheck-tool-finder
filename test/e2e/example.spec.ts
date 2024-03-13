2b2adbb3550fd33796bd74aa380ef2c9a4b91581
Hendrik Schmidt <hendriknas@gmail.com>
2024-03-13 16:44:33 +0100

    Fix tests

--- test/e2e/example.spec.ts
+++ test/e2e/example.spec.ts
@@ -3,7 +3,9 @@ import { expect, test } from "@playwright/test";
 test.describe("test page", () => {
   test("page has title", async ({ page }) => {
     await page.goto("/");
-    await expect(page).toHaveTitle("Digitalcheck Werkzeugfinder");
+    await expect(page).toHaveTitle(
+      "Visualisieren im Digitalcheck - Digitalcheck Werkzeugfinder",
+    );
   });
 
   test("page has h1", async ({ page }) => {
