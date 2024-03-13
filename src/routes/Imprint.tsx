69f408e083675d4090cc0186bcbf4ddf9993dc0d
Hendrik Schmidt <hendriknas@gmail.com>
2024-03-13 12:38:38 +0100

    Refactor Imprint

--- src/routes/Imprint.tsx
+++ src/routes/Imprint.tsx
@@ -18,12 +18,10 @@ function Imprint() {
       </Background>
       <Container paddingTop="48" paddingBottom="48">
         <Box
-          heading={{
-            tagName: "h2",
-            text: "Angaben gemäß § 5 TMG",
-          }}
           content={{
             markdown: `
+## Angaben gemäß § 5 TMG
+
 DigitalService GmbH des Bundes
 Prinzessinnenstraße 8-14  
 10969 Berlin  
@@ -39,27 +37,13 @@ Bundesministerium des Innern und für Heimat
 Handelsregister-Nummer: HRB 212879 B  
 Registergericht: Berlin Charlottenburg  
 Umsatzsteueridentifikationsnummer: DE327075535  
-            `,
-          }}
-        ></Box>
-        <div className={"pb-48"}></div>
-        <Box
-          heading={{
-            tagName: "h2",
-            text: "Kontakt",
-          }}
-          content={{
-            markdown: `E-Mail: digitalcheck@digitalservice.bund.de`,
-          }}
-        ></Box>
-        <div className={"pb-48"}></div>
-        <Box
-          heading={{
-            tagName: "h2",
-            text: "Informationspflichten gemäß Art. 14 ODR-VO, § 36 VSBG",
-          }}
-          content={{
-            markdown: `
+
+## Kontakt
+
+E-Mail: digitalcheck@digitalservice.bund.de
+
+## Informationspflichten gemäß Art. 14 ODR-VO, § 36 VSBG
+
 Die Europäische Kommission stellt eine Plattform zur 
 Online-Streitbeilegung (OS) bereit: https://ec.europa.eu/consumers/odr.
 
@@ -68,17 +52,9 @@ angegebenen Kontaktdaten direkt an uns zu wenden.
 
 Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren 
 vor einer Verbraucherschlichtungsstelle teilzunehmen.
-            `,
-          }}
-        ></Box>
-        <div className={"pb-48"}></div>
-        <Box
-          heading={{
-            tagName: "h2",
-            text: "Haftung für Inhalte",
-          }}
-          content={{
-            markdown: `
+
+## Haftung für Inhalte
+
 Als Diensteanbieter im Sinne von § 2 Nr. 1 TMG sind wir gemäß § 7 Abs. 1 TMG 
 für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. 
 Wir sind nach §§ 8 bis 10 TMG jedoch nicht verpflichtet, übermittelte oder gespeicherte 
@@ -90,17 +66,9 @@ den allgemeinen Gesetzen bleiben hiervon unberührt. Eine dahingehende Haftung i
 jedoch erst ab dem Zeitpunkt möglich, in dem wir Kenntnis von einer konkreten 
 Rechtsverletzung erlangen. Sollten solche Rechtsverletzungen bekannt werden, 
 werden wir die entsprechenden Inhalte umgehend entfernen.
-            `,
-          }}
-        ></Box>
-        <div className={"pb-48"}></div>
-        <Box
-          heading={{
-            tagName: "h2",
-            text: "Haftung für Verweise und Links",
-          }}
-          content={{
-            markdown: `
+
+## Haftung für Verweise und Links
+
 Unser Angebot enthält Links zu externen Websites Dritter, 
 auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese 
 fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten 
@@ -113,17 +81,9 @@ Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne
 konkrete Anhaltspunkte einer Rechtsverletzung nicht mit zumutbarem Aufwand möglich. 
 Sollten Rechtsverletzungen auf verlinkten, externen Seiten bekannt werden, 
 werden wir die entsprechenden Links umgehend entfernen.
-            `,
-          }}
-        ></Box>
-        <div className={"pb-48"}></div>
-        <Box
-          heading={{
-            tagName: "h2",
-            text: "Urheberrecht",
-          }}
-          content={{
-            markdown: `
+
+## Urheberrecht
+
 Die durch uns erstellten Inhalte und Werke auf diesen Seiten unterliegen 
 dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung 
 und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen 
@@ -136,21 +96,13 @@ Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche
 gekennzeichnet. Sollten Sie auf eine Urheberrechtsverletzung aufmerksam werden, 
 bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen 
 werden wir derartige Inhalte umgehend entfernen.
-            `,
-          }}
-        ></Box>
-        <div className={"pb-48"}></div>
-        <Box
-          heading={{
-            tagName: "h2",
-            text: "Verantwortlich für den Inhalt gemäß § 18 Abs. 2 MStV",
-          }}
-          content={{
-            markdown: `DigitalService GmbH des Bundes  
+
+## Verantwortlich für den Inhalt gemäß § 18 Abs. 2 MStV
+
+DigitalService GmbH des Bundes  
 Frau Christina Lang  
 Prinzessinnenstraße 8-14  
-10969 Berlin
-             `,
+10969 Berlin`,
           }}
         ></Box>
       </Container>
