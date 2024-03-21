340526197b505a8558fc9912b3352501af59953a
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-03-21 20:53:58 +0100

    Refactor InputError

--- src/components/InputError.tsx
+++ src/components/InputError.tsx
@@ -7,17 +7,14 @@ type InputErrorProps = PropsWithChildren<{
 
 const InputError = ({ id, children }: InputErrorProps) => {
   return (
-    <div aria-live="assertive">
-      {children && (
-        <div
-          id={id}
-          data-testid="inputError"
-          className="mt-4 text-red-800 flex items-center gap-x-4"
-        >
-          <ErrorOutline className="!w-18 !h-18" />
-          <span className="sr-only">Fehler:</span> {children}
-        </div>
-      )}
+    <div
+      aria-live="assertive"
+      id={id}
+      data-testid="inputError"
+      className="mt-16 text-red-800 flex items-center gap-x-4"
+    >
+      <ErrorOutline className="!w-18 !h-18" />
+      <span className="sr-only">Fehler:</span> {children}
     </div>
   );
 };
