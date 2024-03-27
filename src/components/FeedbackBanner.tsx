2d208c9223f213e46e05cf0ec833c8c89ff23c4a
SabrinaFeuerherd <164863012+SabrinaFeuerherd@users.noreply.github.com>
2024-03-27 12:16:21 +0100

    Update FeedbackBanner.tsx

--- src/components/FeedbackBanner.tsx
+++ src/components/FeedbackBanner.tsx
@@ -10,12 +10,20 @@ export default function FeedbackBanner() {
           heading={{
             tagName: "h2",
             look: "ds-label-01-bold",
-            text: "Haben Sie Fragen oder Anmerkungen?",
+            text: "Es fehlt ein Werkzeug? Sie haben allgemeine Fragen oder Anmerkungen?",
           }}
           content={{
-            markdown: `Diese Seite ist im Aufbau. Kontaktieren Sie uns über digitalcheck@digitalservice.bund.de
-              wenn Ihnen etwas fehlt, oder etwas nicht funktioniert. Ihr Feedback trägt dazu bei, 
-              Informationen und Gestaltung der Webseite für alle Nutzenden zu verbessern.`,
+            markdown: `Dieser Dienst ist im Aufbau. Kontaktieren Sie uns über digitalcheck@digitalservice.bund.de oder  01 51/40 76 78 39, wenn Ihnen etwas fehlt, oder etwas nicht funktioniert. Ihr Feedback trägt dazu bei, Informationen und Gestaltung der Webseite für alle Nutzenden zu verbessern.`,
+          }}
+        ></Box>
+                <Box
+          heading={{
+            tagName: "h2",
+            look: "ds-label-01-bold",
+            text: "Wir suchen Gesprächspartner!",
+          }}
+          content={{
+            markdown: `Um diese Seite weiterzuentwickeln, suchen wir nach Personen, die uns in einem 45-minütigen Gespräch Feedback geben. Schreiben Sie uns gerne eine E-Mail und wir melden uns bei Ihnen: digitalcheck@digitalservice.bund.de`,
           }}
         ></Box>
       </Container>
