37b2caee9643f86ab4bce17d2639b266a2f4d8a0
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-03-27 17:47:59 +0100

    Add subject and body to mails

--- src/components/FeedbackBanner.tsx
+++ src/components/FeedbackBanner.tsx
@@ -3,6 +3,11 @@ import Box from "./Box";
 import Container from "./Container";
 
 export default function FeedbackBanner() {
+  const email = "digitalcheck@digitalservice.bund.de";
+  const subject = "Rückmeldung zum Werkzeugfinder";
+  const interviewText =
+    "Ich stehe gerne für ein Gespräch zur Verfügung, um Feedback zu geben.";
+
   return (
     <Background backgroundColor="midBlue" paddingTop="32" paddingBottom="40">
       <Container paddingTop="0" paddingBottom="0">
@@ -13,7 +18,7 @@ export default function FeedbackBanner() {
             text: "Es fehlt ein Werkzeug? Sie haben allgemeine Fragen oder Anmerkungen?",
           }}
           content={{
-            markdown: `Dieser Dienst ist im Aufbau. Kontaktieren Sie uns über digitalcheck@digitalservice.bund.de oder 0151/40 76 78 39, wenn Ihnen etwas fehlt, oder etwas nicht funktioniert. Ihr Feedback trägt dazu bei, Informationen und Gestaltung der Webseite für alle Nutzenden zu verbessern.`,
+            markdown: `Dieser Dienst ist im Aufbau. Kontaktieren Sie uns über <a href="mailto:${email}?subject=${subject}" class="text-link">${email}</a> oder 0151/40 76 78 39, wenn Ihnen etwas fehlt, oder etwas nicht funktioniert. Ihr Feedback trägt dazu bei, Informationen und Gestaltung der Webseite für alle Nutzenden zu verbessern.`,
           }}
         ></Box>
       </Container>
@@ -25,7 +30,7 @@ export default function FeedbackBanner() {
             text: "Wir suchen Gesprächspartner!",
           }}
           content={{
-            markdown: `Um diese Seite weiterzuentwickeln, suchen wir nach Personen, die uns in einem 45-minütigen Gespräch Feedback geben. Schreiben Sie uns gerne eine E-Mail und wir melden uns bei Ihnen: digitalcheck@digitalservice.bund.de`,
+            markdown: `Um diese Seite weiterzuentwickeln, suchen wir nach Personen, die uns in einem 45-minütigen Gespräch Feedback geben. Schreiben Sie uns gerne eine E-Mail und wir melden uns bei Ihnen: <a href="mailto:${email}?subject=${subject}&body=${interviewText}" class="text-link">${email}</a>.`,
           }}
         ></Box>
       </Container>
