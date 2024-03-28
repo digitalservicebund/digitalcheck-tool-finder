7cc634e9ca3671fd92b237fd6fe8aef38185df36
SabrinaFeuerherd <164863012+SabrinaFeuerherd@users.noreply.github.com>
2024-03-28 17:20:57 +0100

    Update InfoPage.tsx

--- src/routes/InfoPage.tsx
+++ src/routes/InfoPage.tsx
@@ -20,11 +20,7 @@ function InfoPage() {
               text: "Visualisieren im Digitalcheck",
             }}
             content={{
-              markdown: `Sie können die Auswirkungen der Regelung für Betroffene* und die Umsetzung durch die Akteure 
-              des Vollzugs einfacher verstehen, wenn Sie den Vollzug skizzieren – Zusammenhänge werden sichtbar und 
-              Sie können Möglichkeiten der Digitalisierung in der Umsetzung identifizieren. Beginnen Sie die Arbeit 
-              mit der Visualisierung vor der ersten Textarbeit, um Aspekte der Digitaltauglichkeit frühzeitig zu 
-              erkennen und bei der Verschriftlichung Ihrer Regelung berücksichtigen zu können.`,
+              markdown: `Die Visualisierung hilft Ihnen, komplexe Sachverhalte zu strukturieren und dadurch schneller und intuitiver erfassbar zu machen – Zusammenhänge werden sichtbar und Sie können Möglichkeiten der Digitalisierung in der Umsetzung identifizieren. Beginnen Sie die Arbeit mit der Visualisierung vor der ersten Textarbeit, um Aspekte der Digitaltauglichkeit frühzeitig zu erkennen und bei der Verschriftlichung Ihrer Regelung berücksichtigen zu können.`,
             }}
           ></Header>
         </Container>
@@ -35,13 +31,10 @@ function InfoPage() {
           identifier={"info-section-which-tool"}
           heading={{
             tagName: "h2",
-            text: "In welchem Programm visualisieren?",
+            text: "Mit welchem Werkzeug visualisieren?",
           }}
           content={{
-            markdown: `Es existiert eine Vielzahl von Programmen, in denen Visualisierungen angefertigt werden können. 
-            Je nach Detailgrad oder Verwendung kann sich die Auswahl dabei verändern. Zudem ist der Katalog an 
-            verfügbaren Werkzeugen von Ressort zu Ressort unterschiedlich. Hier können Sie das Werkzeug finden, 
-            das für die Visualisierung in Ihrem Regelungsvorhaben das richtige ist.`,
+            markdown: `Es existieren eine Vielzahl von Werkzeuge und Programme, in denen Visualisierungen angefertigt werden können. Zudem ist der Katalog an verfügbaren Werkzeugen von Ressort zu Ressort unterschiedlich. Hier können Sie das Werkzeug finden, welches für Ihren Anlass das richtige ist.`,
           }}
           buttons={[
             {
@@ -81,14 +74,10 @@ Schleifen, uneinheitliche Rechtsbegriffe und mögliche zu vermeidende Schriftfor
           additionalClassNames={"pt-20"}
           heading={{
             tagName: "h3",
-            text: "Die Darstellungen helfen auch bei Verständnis und Umsetzung im Vollzug",
+            text: "Die Darstellungen helfen auch bei der Umsetzung im Vollzug",
           }}
           content={{
-            markdown: `Personen, die Ihre Regelung digital umsetzen (z. B. Programmiererinnen und Programmierer), 
-          denken in Prozessen, Strukturen, Systemen und Zusammenhängen. Mit einer Visualisierung können Sie 
-          diesen beim Verstehen und Übersetzen Ihrer Regelung helfen und so eine bessere Umsetzung ermöglichen. 
-          Darüber hinaus kann Ihre Visualisierung – schon einfache Scans oder Fotos von Skizzen – 
-          Ihren Kolleginnen und Kollegen sowie dem NKR das Verständnis erleichtern.`,
+            markdown: `Personen, die Ihre Regelung digital umsetzen (z. B. Programmiererinnen und Programmierer), denken in Prozessen, Strukturen, Systemen und Zusammenhängen. Mit einer Visualisierung können Sie diesen beim Verstehen und Übersetzen Ihrer Regelung helfen und so eine bessere Umsetzung ermöglichen. Darüber hinaus kann Ihre Visualisierung – schon einfache Scans oder Fotos von Skizzen – Ihren Kolleginnen und Kollegen sowie dem NKR das Verständnis erleichtern.`,
           }}
         ></Box>
         <Background backgroundColor="midBlue" paddingTop="0" paddingBottom="0">
@@ -99,11 +88,7 @@ Schleifen, uneinheitliche Rechtsbegriffe und mögliche zu vermeidende Schriftfor
               text: "Tipp: Schaffen Sie ein gemeinsames Verständnis",
             }}
             content={{
-              markdown: `Tipp: Schaffen Sie ein gemeinsames Verständnis Visualisieren Sie gemeinsam mit 
-          Kolleginnen und Kollegen aus Ihrem Referat, anderen Abteilungen, anderen Expertinnen und 
-          Experten oder Akteuren des Vollzugs, um schnell ein gemeinsames Verständnis von der 
-          Regelung und den Umsetzungsmöglichkeiten zu schaffen. Die Diskussion auf gleicher 
-          Grundlage ist häufig ebenso wertvoll wie die entstandene Visualisierung selbst.`,
+              markdown: `Visualisieren Sie gemeinsam mit Kolleginnen und Kollegen aus Ihrem Referat, anderen Abteilungen, anderen Expertinnen und Experten oder Akteuren des Vollzugs, um schnell ein gemeinsames Verständnis von der Regelung und den Umsetzungsmöglichkeiten zu schaffen. Die Diskussion auf gleicher Grundlage ist häufig ebenso wertvoll wie die entstandene Visualisierung selbst.`,
             }}
           ></Box>
         </Background>
