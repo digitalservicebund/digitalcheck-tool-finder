6c753280d85eae3ad4727d9e1389ee443ae48fce
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-03-28 17:17:56 +0100

    Show text full width without image or alternatives

--- src/components/Recommendation.tsx
+++ src/components/Recommendation.tsx
@@ -35,30 +35,33 @@ export default function renderRecommendation({
               ${tool.link ? "\n\n" + tool.link : ""}
               ${tool.access ? "\n\n" + tool.access : ""}`}
           />
-          <div className="flex-1 space-y-24">
-            {tool.img.src && (
-              <Image
-                url={getImageUrl(tool.img.src)}
-                alternativeText={tool.img.alt}
-              />
-            )}
-            {recommendation.alternativeTools?.length > 0 && (
-              <div className="p-8 bg-gray-100 ds-stack-8 rounded">
-                <h4 className="ds-label-01-bold">Alternativen</h4>
-                <p>
-                  Wenn Sie {tool.name} nicht nutzen können oder möchten, können
-                  Sie ein {clusterName} auch in folgenden Anwendungen erstellen:
-                </p>
-                <ul>
-                  {recommendation.alternativeTools.map((alternative) => (
-                    <li key={`alternative-${alternative.id}`}>
-                      {alternative.name}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
-          </div>
+          {(tool.img.src || recommendation.alternativeTools.length > 0) && (
+            <div className="flex-1 space-y-24">
+              {tool.img.src && (
+                <Image
+                  url={getImageUrl(tool.img.src)}
+                  alternativeText={tool.img.alt}
+                />
+              )}
+              {recommendation.alternativeTools.length > 0 && (
+                <div className="p-8 bg-gray-100 ds-stack-8 rounded">
+                  <h4 className="ds-label-01-bold">Alternativen</h4>
+                  <p>
+                    Wenn Sie {tool.name} nicht nutzen können oder möchten,
+                    können Sie ein {clusterName} auch in folgenden Anwendungen
+                    erstellen:
+                  </p>
+                  <ul>
+                    {recommendation.alternativeTools.map((alternative) => (
+                      <li key={`alternative-${alternative.id}`}>
+                        {alternative.name}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
+          )}
         </div>
       </div>
     </div>
