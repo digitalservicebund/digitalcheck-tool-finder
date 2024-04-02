c6d3276f4e3c555af40c79ec36101522404619ff
SabrinaFeuerherd <164863012+SabrinaFeuerherd@users.noreply.github.com>
2024-04-02 11:39:32 +0200

    Update BIC name

--- resources/data.ts
+++ resources/data.ts
@@ -458,7 +458,7 @@ export default {
     },
     {
       id: "bic",
-      name: "BIC",
+      name: "BIC (wird bald ersetzt durch Adonis)",
       description:
         "BIC Process Design ist eine Spezialsoftware zum Modellieren von Prozessen; die richtige Nutzung braucht Übung. BIC Process Design sollten Sie dann einsetzen, wenn Sie eine FIM-Modellierung anfertigen möchten oder in Zukunft öfter mit FIM-Modellierungen zu tun haben.",
       access:
