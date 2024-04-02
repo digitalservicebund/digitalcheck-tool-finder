2edf15160855344f6230dfe9b8a33a35bb7076c5
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-04-02 17:13:48 +0200

    Enhance unit test

--- test/unit/findResults.spec.ts
+++ test/unit/findResults.spec.ts
@@ -8,7 +8,7 @@ import {
 
 describe("Repository", () => {
   test.each(createTestCases())(
-    'findResultByObjectAndRessort for "%s" and "%s" returns at least one recommended tool.',
+    'findResultByObjectAndRessort for "%s" and "%s" returns two or three recommended tools.',
     (
       ressortName, // used for test name
       objectName, // used for test name
@@ -17,18 +17,32 @@ describe("Repository", () => {
     ) => {
       const result = findResultByObjectAndRessort(object, ressort);
 
+      const ressortsWithoutFlussdiagrammPro = [
+        "aa",
+        "bmvg",
+        "bmel",
+        "bmuv",
+        "bmwsb",
+      ];
+
       expect(result.recommendations).not.toBeNull();
-      expect(result.recommendations.length).toBeGreaterThan(0);
+      if (
+        result.cluster.id === "schaubild" ||
+        (result.cluster.id === "flussdiagramm" &&
+          ressortsWithoutFlussdiagrammPro.includes(ressort.id))
+      ) {
+        expect(result.recommendations.length).toBe(2);
+      } else {
+        expect(result.recommendations.length).toBe(3);
+      }
     },
   );
 });
 
-function createTestCases(): [string, string, Ressort, VisualisationObject][] {
-  const testCases: [string, string, Ressort, VisualisationObject][] = [];
-  getAllRessorts().forEach((ressort) => {
-    getAllObjects().forEach((object) => {
-      testCases.push([ressort.name, object.name, ressort, object]);
-    });
-  });
-  return testCases;
+function createTestCases() {
+  return getAllRessorts().flatMap((ressort) =>
+    getAllObjects().map(
+      (object) => [ressort.name, object.name, ressort, object] as const,
+    ),
+  );
 }
